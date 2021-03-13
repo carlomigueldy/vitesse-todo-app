3e847bc93d40c2dadc27543b2d2510f19348b18a
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-13 20:28:44 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Vitesse Todo App',
+  meta: [
+    { name: 'description', content: 'A simple todo app using Vitesse.' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
